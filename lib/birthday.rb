ce39aa8147ef1507ad9d449a2dbe00484760a743
Neil Empey <argus.two.2@gmail.com>
2020-04-16 16:37:48 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,5 +10,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
+def age_appropriate_birthday(birthday_kids)
+   birthday_kids.each do |key, value|
+     if(value <= 12)
+    print "Happy Birthday #{key}! You are now #{value} years old!\n"
+    end 
+  end
+end
 
 #"Happy Birthday Timmy! You are now 9 years old!
