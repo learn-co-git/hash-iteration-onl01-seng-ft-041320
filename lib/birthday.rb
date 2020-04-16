3d967b369ed86fba8aa7b3cd973af22334fdbd2b
Neil Empey <argus.two.2@gmail.com>
2020-04-16 16:31:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,9 @@
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |key, value|
+    if(value < 12)
     puts "Happy Birthday #{key}! You are now #{value} years old!"
+  end 
   end
 end
 
